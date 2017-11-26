1639f8c1526f0dd7a7825b871687ca35287898ad
Alexander Fridman <alexfridman@outlook.com>
2017-11-26 10:01:49 +0300

    Add mechanism to resume download process

---
+++ update_list.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+
+def load_list(path):
+    with open(path) as f:
+        return set(map(str.strip, filter(None, f)))
+
+
+def load_downloaded_videos(path):
+    def get_f_name(path):
+        name, _ = os.path.splitext(os.path.basename(path))
+        return name
+        
+    return {get_f_name(f) for f in os.listdir(path)}
+    
+    
+def save_list(vid_list, path):
+    with open(path, 'w+') as f:
+        for vid in vid_list:
+            f.write(vid + '\n')
+            
+if __name__ == '__main__':
+    train_vid_list = load_list(sys.argv[2])
+    already_downloaded_train_vid_list = load_downloaded_videos(sys.argv[1])
+    save_list(train_vid_list - already_downloaded_train_vid_list, sys.argv[2])
+
